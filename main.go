3547c21332b50c2b85834d1a9099fa2dbd0502d9
dennougorilla <dennougorilla@gmail.com>
2018-08-19 05:32:12 +0900

    [!]

--- main.go
+++ main.go
@@ -3,24 +3,9 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
-	"golang.org/x/text/encoding/japanese"
-	"golang.org/x/text/transform"
 	"math/rand"
 	"time"
 )
-
-func transformEncoding(rawReader io.Reader, trans transform.Transformer) (string, error) {
-	ret, err := ioutil.ReadAll(transform.NewReader(rawReader, trans))
-	if err == nil {
-		return string(ret), nil
-	} else {
-		return "", err
-	}
-}
-
-func BytesToShiftJIS(b []byte) (string, error) {
-	return transformEncoding(bytes.NewReader(b), japanese.ShiftJIS.NewEncoder())
-}
 
 func main() {
 	bytes := []byte{0xbd, 0xb2, 0x3d, 0xbc, 0x20, 0xe2, 0x8c, 0x98}
@@ -37,12 +22,6 @@ func main() {
 	binary.PutVarint(result, src)
 	fmt.Printf("% x\n", result[0:2])
 	fmt.Println(string(result[0:2]))
-	result, err := BytesToShiftJIS(result[0:2])
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(result)
-
 }
 
 //print(int(random.randrange(0,65535)).to_bytes(2,byteorder='little').decode('shift-jis','replace'), end='')
