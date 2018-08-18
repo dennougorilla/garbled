36058f16041b086155a98800efd29bb4c1763641
dennougorilla <dennougorilla@gmail.com>
2018-08-19 04:06:35 +0900

    [test]

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	bytes := []byte{0x82, 0xA0}
 	s := string(bytes)
-	fmt.Printf("%v: %x\n", s, s)
+	fmt.Printf("% x\n", s)
 	const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
 	fmt.Println(sample)
 	fmt.Printf("% x\n", sample)
