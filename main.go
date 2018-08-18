f57e8bdf75bd417900e9fc00b860c7f817cf5247
dennougorilla <dennougorilla@gmail.com>
2018-08-19 05:09:17 +0900

    [!]

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ func main() {
 	src := int64(65535)
 	result := make([]byte, binary.MaxVarintLen64)
 	binary.PutVarint(result, src)
-	fmt.Printf("% x\n", result)
-	fmt.Println(string(result))
+	fmt.Printf("% x\n", result[0])
+	fmt.Println(string(result[0]))
 }
 
 //print(int(random.randrange(0,65535)).to_bytes(2,byteorder='little').decode('shift-jis','replace'), end='')
